ddb103c43d076672443aee1322400638b8d03c83
Leave-it-blank <70465598+Leave-it-blank@users.noreply.github.com>
2023-04-26 22:30:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['api.lynxscans.com'],
+    domains: ['*.lynxscans.com', '*.ondigitalocean.app', 'lynxscans.com'],
   },
   output: 'standalone',
   
